5b95b263d2fb4bed0aedb5c0ee978498be00578c
Speedkon <131625039+Speedkon@users.noreply.github.com>
2023-12-20 16:58:01 +0000

    Deploying to gh-pages from @ Speedkon/goit-react-hw-08-phonebook@4a80d21004e7a51e2b6ba4de202bfae2aca85d19 🚀

--- static/js/266.22119cbe.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184);function o(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"Wellcome to Phonebook!"})})}}}]);
-//# sourceMappingURL=266.22119cbe.chunk.js.map
